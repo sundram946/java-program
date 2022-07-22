bc1ca09cd02bf19537059bd4e7533fedb0a5ff24
sundram946 <109668620+sundram946@users.noreply.github.com>
2022-07-22 16:46:28 +0530

    Add files via upload

---
+++ operator/arithmetic/single_level1.java
@@ -0,0 +1,10 @@
+package inheritance;
+
+public class single_level1 {
+
+	public void print() {
+		
+		System.out.println("single level inheritance");
+	}
+
+}
---
+++ operator/arithmetic/single_level2.java
@@ -0,0 +1,11 @@
+package inheritance;
+
+public class single_level2 extends single_level1 {
+
+	public static void main(String[] args) {
+
+		single_level1 obj= new single_level1();
+		obj.print();
+		
+	}
+}
