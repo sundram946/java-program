b630734b141356ecb6e1109439bac8aa9c933c71
sundram946 <109668620+sundram946@users.noreply.github.com>
2022-07-28 18:40:00 +0530

    Add files via upload

---
+++ operator/Ternary/ternary_op.java
@@ -0,0 +1,16 @@
+package Operator;
+
+public class ternary_op {
+
+	public static void main(String[] args) {
+		System.out.println("Ternary operator");
+		
+		
+      int a =9,   b=15;
+	System.out.println("the value of a ="+a);
+	System.out.println("the value of  b ="+b);
+int r=(a<b?b:a);
+ System.out.println(r);
+	}
+
+}
