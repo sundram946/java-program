9d57568b88093d5d3038d19397f6de02b749979d
sundram946 <109668620+sundram946@users.noreply.github.com>
2022-07-28 18:34:08 +0530

    Add files via upload

---
+++ operator/assignment/Assignment_op.java
@@ -0,0 +1,32 @@
+package Operator;
+
+public class Assignment_op {
+	
+	public static void main(String[] args) {
+		
+		int a=5;
+		int var;
+		
+		var=a;
+		System.out.println("var = " + var);
+		
+		var +=a;
+		System.out.println("var = " + var);
+		
+		var*=a;
+		System.out.println("var = " + var);
+		var-=a;
+		System.out.println("var="+ var);
+		//var %= a;
+		//System.out.println("var="+var);
+		var /= a;
+		System.out.println("var="+var);
+		
+		
+		
+	
+	
+	
+	}
+
+}
