e4b28125cec3f50a4a338158bb16ad2b27d6558a
sundram946 <109668620+sundram946@users.noreply.github.com>
2022-07-28 18:36:23 +0530

    Add files via upload

---
+++ operator/Bitwise_op.java
@@ -0,0 +1,17 @@
+package Operator;
+
+public class Bitwise_op {
+
+	public static void main(String[] args) {
+	System.out.println("Bitwaise operator");
+	int a = 4; int b = 9;
+	System.out.println("value of a =" +a);
+	System.out.println("value of b =" +b);
+	System.out.println("value of a&b = " +(a&b));
+	System.out.println("value of a|b =" +(a|b));
+	
+	
+
+	}
+
+}
