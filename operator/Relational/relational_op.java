01781ac29c6cef2aea07649920830a2ffc93c73b
sundram946 <109668620+sundram946@users.noreply.github.com>
2022-07-28 18:42:20 +0530

    Add files via upload

---
+++ operator/Relational/relational_op.java
@@ -0,0 +1,27 @@
+package Operator;
+
+public class relational_op {
+
+	public static void main(String[] args) {
+		// create variable 
+		int a =7, b =11;
+		//value of a and b 
+		System.out.println("a is " + a + " and b is "+b);
+		//==operator
+		System.out.println(a==b);//true
+		//!= operator
+		System.out.println(a != b);// true
+		//> operator 
+		System.out.println(a>b); //false
+		//<operator
+		System.out.println(a<b);//true
+		//>= 0perator
+		System.out.println(a>=b);
+		
+		System.out.println(a<=b);
+		
+		
+
+	}
+
+}
