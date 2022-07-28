5c1a3b5938ebc962a7568b9af2026ba309558317
sundram946 <109668620+sundram946@users.noreply.github.com>
2022-07-28 18:35:21 +0530

    Add files via upload

---
+++ operator/arithmetic/Arithmetic_op.java
@@ -0,0 +1,75 @@
+package Operator;
+
+public class Arithmetic_op 
+{
+	public static void main(String[] agrs)// main function
+	{
+//		System.out.println("Arithmetic Operator");
+//		
+////		+
+////		-
+////		*
+////		/
+////		%
+//		
+//		int a, b,c;
+//		
+//		a=2;
+//		b=3;
+//		
+//		c=a+b;
+//		
+//		System.out.println("c");
+		
+	//	System.out.println("arithmetic operator");
+		//{
+			
+			//{
+		
+//       +
+//       -
+//       *
+//       /
+//       %
+//		int a, b, c;
+//		a=5;
+//		b=7;
+//		c=a+b;
+//		System.out.println(c);
+//
+//	//   -
+//	//	System.out.println("a,b,c");
+//	//	a=8;
+//		b=3;
+//		c=a-b;
+//		System.out.println(c);
+//		
+//		a=10;
+//		b=3;
+//		c=a*b;
+//		System.out.println(c);
+//		a=12;
+//		b=6;
+//		c=a/b;
+//		System.out.println(c);
+
+	 
+		System.out.println("simple intrest");
+		int p=40000;
+		int r=5;
+		int t=24;
+		int si=(p*r*t/100);
+		System.out.println( "yfyfuyu= "+si);
+			
+			
+		
+	}
+	
+}
+
+		
+		
+		
+		
+		
+
