fe965c20332f239f98f194471224b9f3d53aa4aa
sundram946 <109668620+sundram946@users.noreply.github.com>
2022-07-28 18:39:12 +0530

    Add files via upload

---
+++ operator/Logical/Logical_op.java
@@ -0,0 +1,17 @@
+package Operator;
+
+public class Logical_op {
+
+	public static void main(String[] args) {
+		
+		int age=5;
+		
+		if(age>18 || age<25)
+			System.out.println("You can vote");
+		
+		else
+			
+			System.out.println("you can't vote");
+	}
+
+}
